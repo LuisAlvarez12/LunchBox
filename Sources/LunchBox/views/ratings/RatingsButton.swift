2fde2cd2d00e1e8a1024001c16b80ffead756c15
Luis <Lalvarez1223@gmail.com>
2023-09-20 14:05:45 -0500

    add ratings button

---
+++ Sources/LunchBox/views/ratings/RatingsButton.swift
@@ -0,0 +1,75 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Luis Alvarez on 9/20/23.
+//
+
+import StoreKit
+import SwiftUI
+
+@available(iOS 16.0, *)
+public struct RatingButton<Content> : View where Content : View{
+   
+    @Environment(\.requestReview) var requestReview
+    
+    @ViewBuilder var content: () -> Content
+    
+    public init(_ content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    public var body: some View {
+        Button(action: {
+            requestReview()
+        }, label: {
+            content()
+        })
+    }
+}
+
+@available(iOS 16.0, *)
+public struct DefaultRatingsButton: View {
+    
+    var text: String
+    var textColor: Color
+    var bgColor: Color
+    
+    public init() {
+        self.text = "Rate This App"
+        self.textColor = Color.LBIdealBluePrimary
+        self.bgColor = Color.LBIdealBlueSecondary
+    }
+    
+    public init(text: String, textColor: Color, bgColor: Color) {
+        self.text = text
+        self.textColor = textColor
+        self.bgColor = bgColor
+    }
+    
+    public var body: some View{
+        RatingButton{
+            Label(text, systemImage: "heart.fill")
+                .foregroundColor(textColor)
+                .fullWidth(ipadWidth: 200)
+                .padding(12)
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .foregroundColor(bgColor)
+                }
+                .horPadding()
+        }
+    }
+}
+
+@available(iOS 16.0, *)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack{
+            DefaultRatingsButton()
+            RatingButton{
+                Text("Leave Review!!")
+            }
+        }
+    }
+}
