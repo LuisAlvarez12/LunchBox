40e9f783d97276bc5bdfcf7acac7d76aac573a5c
Luis Alvarez <Lalvarez1223@gmail.com>
2023-09-26 09:43:59 -0500

    icon updates

--- Sources/LunchBox/views/screen/NoticeView.swift
+++ Sources/LunchBox/views/screen/NoticeView.swift
@@ -8,26 +8,56 @@
 import SwiftUI
 
 @available(iOS 16.0, *)
+public struct IconProperties {
+    var size: CGFloat = 100
+    var background: Color? = nil
+    var backgroundGradient: Bool = true
+}
+
+@available(iOS 16.0, *)
 public struct NoticeView: View {
     @Environment(\.colorScheme) var colorScheme
 
     let image: String
     let title: LocalizedStringKey
     var bodyText: LocalizedStringKey? = nil
+    
+    var iconProperties = IconProperties(size: 100)
 
     public init(image: String, title: LocalizedStringKey, bodyText: LocalizedStringKey? = nil) {
         self.image = image
         self.title = title
         self.bodyText = bodyText
     }
+    
+    public init(image: String, title: LocalizedStringKey, bodyText: LocalizedStringKey? = nil, iconProperties: IconProperties) {
+        self.image = image
+        self.title = title
+        self.bodyText = bodyText
+        self.iconProperties = iconProperties
+    }
 
     public var body: some View {
         VStack {
             Spacer()
-            Image(image)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100)
+            if let _iconProp =  iconProperties.background {
+                Image(image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: iconProperties.size)
+                    .background(content: {
+                        if iconProperties.backgroundGradient {
+                            Circle().foregroundStyle( _iconProp.gradient)
+                        } else {
+                            Circle().foregroundStyle( _iconProp)
+                        }
+                    })
+            } else {
+                Image(image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: iconProperties.size)
+            }
 
             Text(title).font(.system(size: 18, weight: .bold, design: .default))
                 .foregroundColor(Color.LBMonoScreenOffTone)
