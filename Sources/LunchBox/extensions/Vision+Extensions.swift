f73bedd03ce0e33b5850577bfdeeab59976e84dc
Luis Alvarez <Lalvarez1223@gmail.com>
2024-01-27 12:20:45 -0600

    lunchbox compatabile

--- Sources/LunchBox/extensions/Vision+Extensions.swift
+++ Sources/LunchBox/extensions/Vision+Extensions.swift
@@ -21,10 +21,31 @@ public extension Color {
     }
 }
 
+public enum OrnamentCompatibleAnchor {
+    case leading
+    case trailing
+    case bottom
+    case top
+    
+    @available(visionOS 1.0, *)
+    public var anchor: OrnamentAttachmentAnchor {
+        switch self {
+        case .leading:
+                .scene(.leading)
+        case .trailing:
+                .scene(.trailing)
+        case .bottom:
+                .scene(.bottom)
+        case .top:
+                .scene(.top)
+        }
+    }
+}
+
 public extension View {
-    func lunchboxOrnament<Content>(visibility: Visibility = .automatic, attachmentAnchor: OrnamentAttachmentAnchor, contentAlignment: Alignment = .center, @ViewBuilder ornament: () -> Content) -> some View where Content: View {
+    func lunchboxOrnament<Content>(visibility: Visibility = .automatic, attachmentAnchor: OrnamentCompatibleAnchor, contentAlignment: Alignment = .center, @ViewBuilder ornament: () -> Content) -> some View where Content: View {
         #if os(visionOS)
-            self.ornament(visibility: visibility, attachmentAnchor: attachmentAnchor, contentAlignment: contentAlignment, ornament: ornament)
+        self.ornament(visibility: visibility, attachmentAnchor: attachmentAnchor.anchor, contentAlignment: contentAlignment, ornament: ornament)
         #else
             self
         #endif
