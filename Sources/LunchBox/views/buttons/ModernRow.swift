2dcd55947e69cdb67b24439a6832f31948d4cd34
Luis Alvarez <Lalvarez1223@gmail.com>
2024-05-15 07:44:58 -0500

    add new buttons

---
+++ Sources/LunchBox/views/buttons/ModernRow.swift
@@ -0,0 +1,55 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Luis Alvarez on 5/15/24.
+//
+
+import SwiftUI
+
+public struct ModernRow: View {
+    
+    public let text: LocalizedStringKey
+    public let image: String
+    public var color: Color
+    
+    public init(text: LocalizedStringKey, image: String, color: Color) {
+        self.text = text
+        self.image = image
+        self.color = color
+    }
+    
+    public var body: some View {
+        HStack{
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .squareFrame(length: 24)
+                .foregroundStyle(Color.white)
+                .bold()
+                .padding(8)
+                .background {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(color)
+                }.padding(12)
+                
+            Text(text)
+                .foregroundStyle(Color.LBMonoScreenOffTone)
+                .font(.title3)
+                .aligned()
+                .bold()
+             
+            Image(systemName: "chevron.right")
+                .foregroundStyle(Color.LBMonoScreenOffTone)
+                .font(.title3)
+                .padding(.trailing)
+        }
+        .background{
+            Color.systemSecondary
+        }.clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+}
+
+#Preview {
+    ModernRow(text: "This is a test", image: "house", color: .red)
+}
---
+++ Sources/LunchBox/views/buttons/ScrollingPrimaryButton.swift
@@ -0,0 +1,59 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Luis Alvarez on 5/15/24.
+//
+
+import SwiftUI
+
+public struct ScrollingPrimaryButton: View {
+    
+    public var expanded = true
+    public var title: String
+    public var icon: String
+    public var color: Color
+    public var textColor: Color = .white
+
+    public init(expanded: Bool = true, title: String, icon: String, color: Color, textColor: Color) {
+        self.expanded = expanded
+        self.title = title
+        self.icon = icon
+        self.color = color
+        self.textColor = textColor
+    }
+    
+    public var body: some View {
+        HStack{
+            if expanded {
+                Text(title)
+                    .lineLimit(1)
+                    .fixedSize()
+                    .bold()
+            }
+            Image(systemName: icon)
+                .font(.title3)
+                .bold()
+        }
+        .foregroundStyle(.white)
+            .padding()
+            .background {
+                Capsule().fill(color.gradient)
+            }
+    }
+}
+
+#Preview {
+    VStack{
+        @State var buttonEnabled = true
+        
+        Button("toggle") {
+            withAnimation {
+                buttonEnabled.toggle()
+            }
+        }
+        
+        ScrollingPrimaryButton(expanded: buttonEnabled, title: "Add File", icon: "plus", color: .blue, textColor: .white)
+    }
+    
+}
